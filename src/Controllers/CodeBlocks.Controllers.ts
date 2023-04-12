f832781a53c08e5ac51ba98be8628e1a1d10d2df
Yuval Weiss <108930250+YuvalWeiss1403@users.noreply.github.com>
2023-04-13 00:06:53 +0300

    update env file

--- src/Controllers/CodeBlocks.Controllers.ts
+++ src/Controllers/CodeBlocks.Controllers.ts
@@ -4,9 +4,7 @@ import { Request, Response } from "express";
 export const getAllCodeblocks = async (req: Request, res: Response) => {
 	try {
 		const codeBlocks = await getAllCodeBlocks();
-		return res.status(200).json({
-			codeBlocks,
-		});
+		return res.status(200).json(codeBlocks);
 	} catch (err) {
 		throw err;
 	}
--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 import codeBlocks from "../Routes/CodeBlocks.Routes";
 const router = express.Router();
 
-router.use("/CodeBlock", codeBlocks);
+router.use("/codeblock", codeBlocks);
 
 export default router;
