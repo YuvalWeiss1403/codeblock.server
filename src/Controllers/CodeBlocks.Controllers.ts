d713a2fcff6b8da39c355cd70ed277b52c007508
Yuval Weiss <108930250+YuvalWeiss1403@users.noreply.github.com>
2023-04-12 22:40:21 +0300

    code block controller

---
+++ src/Controllers/CodeBlocks.Controllers.ts
@@ -0,0 +1,13 @@
+import { getAllCodeBlocks } from "../Services/CodeBlocks.Services";
+import { Request, Response } from "express";
+
+export const getAllCodeblocks = async (req: Request, res: Response) => {
+	try {
+		const codeBlocks = await getAllCodeBlocks();
+		return res.status(200).json({
+			codeBlocks,
+		});
+	} catch (err) {
+		throw err;
+	}
+};
